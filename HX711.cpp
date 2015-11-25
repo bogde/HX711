1f9aaee116ce9463bbc4a310beea4d24d0ff7a1b
Zachary J. Fields <zachary_fields@yahoo.com>
2015-11-24 17:24:02 -0800

    Correctly handle two's complement value

--- HX711.cpp
+++ HX711.cpp
@@ -40,7 +40,9 @@ long HX711::read() {
 	// wait for the chip to become ready
 	while (!is_ready());
 
-	byte data[3];
+    unsigned long value = 0;
+    byte data[3] = { 0 };
+    byte filler = 0x00;
 
 	// pulse the clock pin 24 times to read the data
     data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
@@ -48,14 +50,32 @@ long HX711::read() {
     data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
 
 	// set the channel and the gain factor for the next reading using the clock pin
-	for (int i = 0; i < GAIN; i++) {
+	for (unsigned int i = 0; i < GAIN; i++) {
 		digitalWrite(PD_SCK, HIGH);
 		digitalWrite(PD_SCK, LOW);
 	}
 
-	data[2] ^= 0x80;
+    // Datasheet indicates the value is returned as a two's complement value
+    // Flip all the bits
+    data[2] = ~data[2];
+    data[1] = ~data[1];
+    data[0] = ~data[0];
 
-	return ((uint32_t) data[2] << 16) | ((uint32_t) data[1] << 8) | (uint32_t) data[0];
+    // Replicate the most significant bit to pad out a 32-bit signed integer
+    if ( data[2] & 0x80 ) {
+        filler = 0xFF;
+    } else {
+        filler = 0x00;
+    }
+
+    // Construct a 32-bit signed integer
+    value = ( static_cast<unsigned long>(filler) << 24
+            | static_cast<unsigned long>(data[2]) << 16
+            | static_cast<unsigned long>(data[1]) << 8
+            | static_cast<unsigned long>(data[0]) );
+
+    // ... and add 1
+    return static_cast<long>(++value);
 }
 
 long HX711::read_average(byte times) {
