c51320862b26e21d5d9a64cba219dc8ac092e9a3
Tal Achituv <tal@compugician.com>
2016-09-15 17:34:10 -0400

    change to implement multiple sensors

--- HX711-multi.cpp
+++ HX711-multi.cpp
@@ -1,32 +1,34 @@
 #include <Arduino.h>
-#include <HX711.h>
+#include <HX711-multi.h>
 
-HX711::HX711(byte dout, byte pd_sck, byte gain) {
-	begin(dout, pd_sck, gain);
-}
-
-HX711::HX711() {
-}
-
-HX711::~HX711() {
-
-}
-
-void HX711::begin(byte dout, byte pd_sck, byte gain) {
-	PD_SCK = pd_sck;
-	DOUT   = dout;
+HX711MULTI::HX711MULTI(int count, byte *dout, byte pd_sck, byte gain) {
+	PD_SCK 	= pd_sck;
+	DOUT 	= dout; //TODO - make the input of dout to the function a const, or otherwise copy the values for local storage
+	COUNT   = count;
 
 	pinMode(PD_SCK, OUTPUT);
-	pinMode(DOUT, INPUT);
-
+	for (int i=0; i<count; i++) {
+		pinMode(DOUT[i], INPUT);
+	}
 	set_gain(gain);
 }
 
-bool HX711::is_ready() {
-	return digitalRead(DOUT) == LOW;
+HX711MULTI::~HX711MULTI() {
+
 }
 
-void HX711::set_gain(byte gain) {
+bool HX711MULTI::is_ready() { 
+	bool result = true;
+	for (int i = 0; i<COUNT; ++i) {
+		if (digitalRead(DOUT[i]) == HIGH) {
+			result = false;
+		}
+	}
+	return result;
+}
+
+void HX711MULTI::set_gain(byte gain) {
+
 	switch (gain) {
 		case 128:		// channel A, gain factor 128
 			GAIN = 1;
@@ -40,99 +42,54 @@ void HX711::set_gain(byte gain) {
 	}
 
 	digitalWrite(PD_SCK, LOW);
-	read();
+	read(); //a read is needed to get gain setting to come into effect. (for the next read)
 }
 
-long HX711::read() {
-	// wait for the chip to become ready
-	while (!is_ready()) {
-		// Will do nothing on Arduino but prevent resets of ESP8266 (Watchdog Issue)
-		yield();
-	}
+byte HX711MULTI::get_count() {
+	return COUNT;
+}
 
-    unsigned long value = 0;
-    byte data[3] = { 0 };
-    byte filler = 0x00;
+//reads from all cahnnels and sets the values into the passed long array pointer (which must have at least 'count' cells allocated)
+//send NULL if you are only reading to toggle the line, and not to get values, such as in the case of setting gains.
+void HX711MULTI::read(long *result) {
+	int i,j;
+	// wait for all the chips to become ready
+	while (!is_ready());
 
 	// pulse the clock pin 24 times to read the data
-    data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-    data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-    data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-
+	for (i = 0; i < 24; ++i) {
+		digitalWrite(PD_SCK, HIGH);
+		if (NULL!=result) {
+			for (j = 0; j < COUNT; ++j) {
+				bitWrite(result[j], 23-i, digitalRead(DOUT[j]));
+			}
+		}
+		digitalWrite(PD_SCK, LOW);
+	}
+   
 	// set the channel and the gain factor for the next reading using the clock pin
-	for (unsigned int i = 0; i < GAIN; i++) {
+	for (i = 0; i < GAIN; ++i) {
 		digitalWrite(PD_SCK, HIGH);
 		digitalWrite(PD_SCK, LOW);
 	}
 
-    // Datasheet indicates the value is returned as a two's complement value
-    // Flip all the bits
-    data[2] = ~data[2];
-    data[1] = ~data[1];
-    data[0] = ~data[0];
-
-    // Replicate the most significant bit to pad out a 32-bit signed integer
-    if ( data[2] & 0x80 ) {
-        filler = 0xFF;
-    } else if ((0x7F == data[2]) && (0xFF == data[1]) && (0xFF == data[0])) {
-        filler = 0xFF;
-    } else {
-        filler = 0x00;
+    // Datasheet indicates the value is returned as a two's complement value, so 'stretch' the 24th bit to fit into 32 bits. 
+    if (NULL!=result) {
+	    for (int j = 0; j < COUNT; ++j) {
+	    	if ( ( result[j] & 0x00800000 ) ) {
+	    		result[j] |= 0xFF000000;
+	    	} else {
+	    		result[j] &= 0x00FFFFFF; //required in lieu of re-setting the value to zero before shifting bits in.
+	    	}
+	    }    	
     }
-
-    // Construct a 32-bit signed integer
-    value = ( static_cast<unsigned long>(filler) << 24
-            | static_cast<unsigned long>(data[2]) << 16
-            | static_cast<unsigned long>(data[1]) << 8
-            | static_cast<unsigned long>(data[0]) );
-
-    // ... and add 1
-    return static_cast<long>(++value);
 }
 
-long HX711::read_average(byte times) {
-	long sum = 0;
-	for (byte i = 0; i < times; i++) {
-		sum += read();
-		yield();
-	}
-	return sum / times;
-}
-
-double HX711::get_value(byte times) {
-	return read_average(times) - OFFSET;
-}
-
-float HX711::get_units(byte times) {
-	return get_value(times) / SCALE;
-}
-
-void HX711::tare(byte times) {
-	double sum = read_average(times);
-	set_offset(sum);
-}
-
-void HX711::set_scale(float scale) {
-	SCALE = scale;
-}
-
-float HX711::get_scale() {
-	return SCALE;
-}
-
-void HX711::set_offset(long offset) {
-	OFFSET = offset;
-}
-
-long HX711::get_offset() {
-	return OFFSET;
-}
-
-void HX711::power_down() {
+void HX711MULTI::power_down() {
 	digitalWrite(PD_SCK, LOW);
 	digitalWrite(PD_SCK, HIGH);
 }
 
-void HX711::power_up() {
+void HX711MULTI::power_up() {
 	digitalWrite(PD_SCK, LOW);
 }
